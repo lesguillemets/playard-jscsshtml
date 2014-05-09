7f37ea8a7353f34cbd49eaa421ecef675986a8ef
exumbra_insolem <lesguillemets@users.noreply.github.com>
2014-05-09 19:11:23 +0900

    better colouring

--- canvas_exp/js/langton0.js
+++ canvas_exp/js/langton0.js
@@ -14,12 +14,15 @@ function World(world, dotsize){
 
 World.prototype.createAnts = function(n){
   for (var i=0; i<n; i++){
-    var color = "rgba(100," + (255*i/n).toString() + ","+
-      (255-255*i/n).toString() + ",0.8)";
+    var cbase = 255*i/n;
+    var color0 = "rgb(100," + cbase.toString() + ","+
+      (255-cbase).toString() + ")";
+    var color1 = "rgb(200," + (Math.max(cbase+50,256)).toString() + ","+
+      (Math.max(305-cbase,256)).toString() + ")";
     var x = Math.floor(Math.random() * this.w);
     var y = Math.floor(Math.random() * this.h);
     var drc = Math.floor(Math.random() * 4);
-    this.ants.push(new Ant(x,y,drc,color));
+    this.ants.push(new Ant(x,y,drc,color0,color1));
     //console.log(this.ants[this.ants.length-1].toString());
   }
 };
@@ -31,6 +34,14 @@ World.prototype.moveAnts = function(){
     var ant = this.ants[i];
     if (ant.isalive){
       var vect = ant.drcs[ant.drc];
+      // paint current position
+      if (!!this.grid[ant.x][ant.y]){
+        this.cnt.fillStyle = ant.color0;
+      } else {
+        this.cnt.fillStyle = ant.color1;
+      }
+      this.cnt.fillRect(
+        ant.x*this.dotsize,ant.y*this.dotsize, this.dotsize, this.dotsize);
       // move
       ant.x += vect[0];
       ant.y += vect[1];
@@ -49,19 +60,20 @@ World.prototype.moveAnts = function(){
       }
       // then mark the field
       this.grid[x][y] = !this.grid[x][y]
-      // and show
-      this.cnt.fillStyle = ant.color;
+      // and show (current position shown in ccolor)
+      this.cnt.fillStyle = ant.ccolor;
       this.cnt.fillRect(
         x*this.dotsize,y*this.dotsize, this.dotsize, this.dotsize);
     }
   }
 };
 
-function Ant(x,y,drc,color){
+function Ant(x,y,drc,color0,color1){
   this.x = x;
   this.y = y;
   this.drc = drc; // drc: 0 (up) 1 (right) 2 (down) 3 (right)
-  this.color = color; // some object that can be set to cnt.fillStyle
+  this.color0 = color0; // some object that can be set to cnt.fillStyle
+  this.color1 = color1; // for "visited but off"
   this.isalive = true;
 }
 
@@ -71,6 +83,8 @@ Ant.prototype.toString = function(){
   return ("Ant at " + this.x + ", " + this.y + " heading " + this.drc
   + " with colour: " + this.color);
 };
+
+Ant.prototype.ccolor = "rgb(0,0,0)";
 
 
 function init(){
