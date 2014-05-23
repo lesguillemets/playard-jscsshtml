23e87ce1d61074748bcf5288210fbf92d49a5ead
lesguillemets <lesguillemets@users.noreply.github.com>
2014-05-23 22:22:13 +0900

    ga0 -- now working

--- algorithms/js/ga0.js
+++ algorithms/js/ga0.js
@@ -1,10 +1,16 @@
 var citysize = 5; // show cities as 5*5 [px*px] square.
+var showEvery = 50;  // show infos every 50 generations.
+
 function Simulator(){
+  // essentially:
+  // { c::Int, n::Int, m::Int, p::Floating, g::Int, canv::Canvas}
   this.c = document.getElementById("c").value; // number of cities
   this.n = document.getElementById("n").value; // size of the population
   this.m = document.getElementById("m").value; // pairs make child
   this.p = document.getElementById("p").value; // mutation probability
   this.g = document.getElementById("g").value; // how many years?
+  this.showFlag = 0;
+  this.gen = 0;
   this.canv = document.getElementById("path");
   this.ctx = this.canv.getContext("2d");
   this.mapWidth = this.canv.width;
@@ -23,6 +29,8 @@ function Simulator(){
   // evaluate, and then n again after selection.
 }
 
+// required methods for initialization.
+
 Simulator.prototype.generateCities = function(){
   for (var n=0; n<this.c; n++){
     this.cities[n] = [
@@ -30,18 +38,6 @@ Simulator.prototype.generateCities = function(){
       Math.floor(Math.random()*this.mapHeight)
     ];
   }
-};
-
-Simulator.prototype.drawCities = function(){
-    this.ctx.fillStyle = "rgb(0,0,0)";
-    for (var i=0; i<this.cities.length; i++){
-      var city = this.cities[i];
-      this.ctx.fillRect(
-        city[0]-citysize/2, city[1]-citysize/2,
-        citysize,citysize
-      );
-      //console.log(city);
-    }
 };
 
 Simulator.prototype.makeDistMatrix = function(){
@@ -60,9 +56,51 @@ Simulator.prototype.generateFirstGen = function(){
   }
 };
 
-// required methods for initialization.
+// main loop.
+Simulator.prototype.mainLoop = function(){
+  for(this.gen=0; this.gen<this.g; this.gen++){
+    this.makeChildren();
+    this.applyMutations();
+    this.evalIndividuals();
+    this.selectNextGen();
+    if(! this.showFlag--){
+      this.showFlag = showEvery;
+      this.pushtoTable();
+      this.clearPathCanv();
+      this.drawCities();
+      this.drawPath(0);
+    }
+  }
+}
 
-Simulator.prototype.calcTotalDistances = function(){
+// methods required for each step.
+
+Simulator.prototype.makeChildren = function(){
+  // when calling this function, this.population.length should be n.
+  for(var pair=0; pair<this.m; pair++){
+    var papa = this.population[Math.floor(Math.random()*this.n)];
+    var mama = this.population[Math.floor(Math.random()*this.n)];
+    this.population.push(helloChild(papa,mama));
+    this.population.push(helloChild(mama,papa));
+  }
+};
+
+Simulator.prototype.applyMutations = function(){
+  for (var i=0; i<this.population.length; i++){
+    if (Math.random() < this.p){
+      // add mutation.
+      var gene = this.population[i].route;
+      var first = gene.splice(0,Math.floor(Math.random()*gene.length));
+      var middle = gene.splice(0,Math.floor(Math.random()*gene.length));
+      middle.reverse();  // elddim
+      middle = middle.concat(gene);  // elddimglast
+      gene = first.concat(middle)  // firstelddimglast
+      this.population[i].route = gene;
+    }
+  }
+};
+
+Simulator.prototype.evalIndividuals = function(){
   for (var i=0; i<this.population.length; i++){
     var thisguy = this.population[i];
     var totalDist = 0;
@@ -76,6 +114,63 @@ Simulator.prototype.calcTotalDistances = function(){
     this.population[i].totalDist = totalDist;
     this.population[i].value = 100/(totalDist*totalDist);
   }
+  // sort by value descending.
+  this.population.sort(function(a,b){return b.value-a.value;});
+};
+
+Simulator.prototype.selectNextGen = function(){
+  // Simulator.prototype.evalIndividuals sorts this.generation
+  // prior to this function.
+  // elitism.
+  this.population = this.population.slice(0,this.n);
+};
+
+// methods required for visualisation.
+
+Simulator.prototype.clearPathCanv = function(){
+  this.canv.width = this.canv.width;
+};
+
+Simulator.prototype.drawCities = function(){
+    this.ctx.fillStyle = "rgb(0,0,0)";
+    for (var i=0; i<this.cities.length; i++){
+      var city = this.cities[i];
+      this.ctx.fillRect(
+        city[0]-citysize/2, city[1]-citysize/2,
+        citysize,citysize
+      );
+      //console.log(city);
+    }
+};
+
+Simulator.prototype.drawPath = function(invn){
+  // Int (individual's number s.t. this.population[i] is him
+  // -> IO()
+  this.ctx.fillStyle = "#00FF99";
+  var path = this.population[invn].route;
+  var city = this.cities[path[0]]
+  this.ctx.moveTo(city[0],city[1]);
+  for (var cityn=1; cityn<path.length;cityn++){
+    city = this.cities[path[cityn]]
+    this.ctx.lineTo(city[0],city[1]);
+  }
+  this.ctx.stroke();
+};
+
+Simulator.prototype.drawGraph = function () {
+}
+
+Simulator.prototype.pushtoTable = function(){
+  var tbl = document.getElementById("results");
+  var newrow = tbl.insertRow(1);
+  var c0 = newrow.insertCell(0);
+  var c1 = newrow.insertCell(1);
+  var c2 = newrow.insertCell(2);
+  var c3 = newrow.insertCell(3);
+  c0.innerHTML = this.gen;
+  c1.innerHTML = this.population[0].totalDist;
+  c2.innerHTML = this.population[this.n-1].totalDist;
+  c3.innerHTML = "None";
 };
 
 function Individual(route){
@@ -107,23 +202,18 @@ function randIndividual(n){
   return new Individual(randomPerm);
 }
 
-function makeChild(n){
-}
-
-function tablehandler(){
-  var tbl = document.getElementById("results");
-  var newrow = tbl.insertRow(1);
-  var c0 = newrow.insertCell(0);
-  var c1 = newrow.insertCell(1);
-  var c2 = newrow.insertCell(2);
-  var c3 = newrow.insertCell(3);
-  c0.innerHTML = Math.random();
-  c1.innerHTML = Math.random();
-  c2.innerHTML = Math.random();
-  c3.innerHTML = Math.random();
+function helloChild(papa,mama){
+  var crossloc = Math.floor(Math.random()*papa.route.length);
+  var nextgene = papa.route.slice(0,crossloc);
+  var lowerhalf = papa.route.slice(crossloc);
+  lowerhalf.sort(
+    function(a,b){return mama.route.indexOf(a)-mama.route.indexOf(b)}
+  );
+  nextgene = nextgene.concat(lowerhalf);
+  return new Individual(nextgene);
 }
 
 function main(){
   sim = new Simulator();
-  sim.drawCities();
+  sim.mainLoop();
 }
