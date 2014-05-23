7e322660137ef68cc084aa59ac7e780e253bb4f3
lesguillemets <lesguillemets@users.noreply.github.com>
2014-05-23 15:02:19 +0900

    create table for logging

--- algorithms/js/ga0.js
+++ algorithms/js/ga0.js
@@ -1,2 +1,12 @@
 function main(){
+  var tbl = document.getElementById("results");
+  var newrow = tbl.insertRow(1);
+  var c0 = newrow.insertCell(0);
+  var c1 = newrow.insertCell(1);
+  var c2 = newrow.insertCell(2);
+  var c3 = newrow.insertCell(3);
+  c0.innerHTML = Math.random();
+  c1.innerHTML = Math.random();
+  c2.innerHTML = Math.random();
+  c3.innerHTML = Math.random();
 }
