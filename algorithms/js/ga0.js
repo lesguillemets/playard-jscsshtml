9d4c716821905655936a91eb5bd28c214e43abc9
lesguillemets <lesguillemets@users.noreply.github.com>
2014-05-23 18:39:31 +0900

    alg/ga0 ; create first generation.

--- algorithms/js/ga0.js
+++ algorithms/js/ga0.js
@@ -7,8 +7,8 @@ function Simulator(){
   this.g = document.getElementById("g").value; // how many years?
   this.canv = document.getElementById("path");
   this.ctx = this.canv.getContext("2d");
-  this.mapwidth = 500;
-  this.mapheight = 500;
+  this.mapWidth = this.canv.width;
+  this.mapHeight = this.canv.height;
   this.cities = new Array(this.c); // c City objects.
   this.generateCities();
   this.distances = new Array(this.c);
@@ -16,7 +16,9 @@ function Simulator(){
     this.distances[i] = new Array(this.c); // c*c numbers.
     // TODO : make this c*c/2?
   }
+  this.makeDistMatrix();
   this.population = new Array();
+  this.generateFirstGen();
   // first n members, then n+2*m after gaining child,
   // evaluate, and then n again after selection.
 }
@@ -24,8 +26,8 @@ function Simulator(){
 Simulator.prototype.generateCities = function(){
   for (var n=0; n<this.c; n++){
     this.cities[n] = [
-      Math.floor(Math.random()*this.mapwidth),
-      Math.floor(Math.random()*this.mapheight)
+      Math.floor(Math.random()*this.mapWidth),
+      Math.floor(Math.random()*this.mapHeight)
     ];
   }
 };
@@ -38,9 +40,75 @@ Simulator.prototype.drawCities = function(){
         city[0]-citysize/2, city[1]-citysize/2,
         citysize,citysize
       );
-      console.log(city);
+      //console.log(city);
     }
 };
+
+Simulator.prototype.makeDistMatrix = function(){
+  for (var i=0; i<this.c; i++){
+    for(var j=i; j<this.c; j++){
+      var dist = calcDist(this.cities[i], this.cities[j]);
+      this.distances[i][j] = dist;
+      this.distances[j][i] = dist;
+    }
+  }
+};
+
+Simulator.prototype.generateFirstGen = function(){
+  for(var i=0; i<this.n; i++){
+    this.population[i] = randIndividual(this.c);
+  }
+};
+
+// required methods for initialization.
+
+Simulator.prototype.calcTotalDistances = function(){
+  for (var i=0; i<this.population.length; i++){
+    var thisguy = this.population[i];
+    var totalDist = 0;
+    var prevCityNum = thisguy.route[0];
+    var crntCityNum = -1;
+    for (var cn=1; cn<this.cities.length; cn++){
+      crntCityNum = thisguy.route[cn];
+      totalDist += this.distances[prevCityNum][crntCityNum];
+      prevCityNum = crntCityNum;
+    }
+    this.population[i].totalDist = totalDist;
+    this.population[i].value = 100/(totalDist*totalDist);
+  }
+};
+
+function Individual(route){
+  this.route = route;
+  this.totalDist = 0;
+  this.value = 0;
+}
+
+function calcDist(city1, city2){
+  // :: (Num t) => City -> City -> t
+  var deltaX = city1[0] - city2[0];
+  var deltaY = city1[1] - city2[1];
+  return Math.sqrt(deltaX*deltaX + deltaY*deltaY);
+}
+
+function randIndividual(n){
+  // :: Int -> Individual where n is the number of cities.
+  var stack = [];
+  var randomPerm = [];
+  for (var i=0; i<n; i++){
+    stack[i] = i;
+  }
+  var remaining = n;
+  while(remaining>0){
+    var index = Math.floor(Math.random()*remaining);
+    randomPerm.push(stack.splice(index,1)[0]);
+    remaining--;
+  }
+  return new Individual(randomPerm);
+}
+
+function makeChild(n){
+}
 
 function tablehandler(){
   var tbl = document.getElementById("results");
