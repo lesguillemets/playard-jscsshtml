9e09a4bcc80286fcf3f9c38491a505db8d60bbb6
lesguillemets <lesguillemets@users.noreply.github.com>
2014-05-23 15:45:25 +0900

    steps

--- algorithms/js/ga0.js
+++ algorithms/js/ga0.js
@@ -1,4 +1,48 @@
-function main(){
+var citysize = 5; // show cities as 5*5 [px*px] square.
+function Simulator(){
+  this.c = document.getElementById("c").value; // number of cities
+  this.n = document.getElementById("n").value; // size of the population
+  this.m = document.getElementById("m").value; // pairs make child
+  this.p = document.getElementById("p").value; // mutation probability
+  this.g = document.getElementById("g").value; // how many years?
+  this.canv = document.getElementById("path");
+  this.ctx = this.canv.getContext("2d");
+  this.mapwidth = 500;
+  this.mapheight = 500;
+  this.cities = new Array(this.c); // c City objects.
+  this.generateCities();
+  this.distances = new Array(this.c);
+  for (var i=0; i<this.c; i++){
+    this.distances[i] = new Array(this.c); // c*c numbers.
+    // TODO : make this c*c/2?
+  }
+  this.population = new Array();
+  // first n members, then n+2*m after gaining child,
+  // evaluate, and then n again after selection.
+}
+
+Simulator.prototype.generateCities = function(){
+  for (var n=0; n<this.c; n++){
+    this.cities[n] = [
+      Math.floor(Math.random()*this.mapwidth),
+      Math.floor(Math.random()*this.mapheight)
+    ];
+  }
+};
+
+Simulator.prototype.drawCities = function(){
+    this.ctx.fillStyle = "rgb(0,0,0)";
+    for (var i=0; i<this.cities.length; i++){
+      var city = this.cities[i];
+      this.ctx.fillRect(
+        city[0]-citysize/2, city[1]-citysize/2,
+        citysize,citysize
+      );
+      console.log(city);
+    }
+};
+
+function tablehandler(){
   var tbl = document.getElementById("results");
   var newrow = tbl.insertRow(1);
   var c0 = newrow.insertCell(0);
@@ -9,4 +53,9 @@ function main(){
   c1.innerHTML = Math.random();
   c2.innerHTML = Math.random();
   c3.innerHTML = Math.random();
+}
+
+function main(){
+  sim = new Simulator();
+  sim.drawCities();
 }
