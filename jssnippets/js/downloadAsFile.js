ba3a5adf5a94a767f70fe6553297a6c428e8fe8a
lesguillemets <lesguillemets@users.noreply.github.com>
2014-06-01 11:56:42 +0900

    downloadAsFile now supports firefox.

--- jssnippets/js/downloadAsFile.js
+++ jssnippets/js/downloadAsFile.js
@@ -1,4 +1,4 @@
-// ref:http://stackoverflow.com/questions/19679842
+// create sample object
 var myObject = new Array(10);
 for (var i=0; i<myObject.length; i++){
   myObject[i] = new Array(4);
@@ -6,13 +6,20 @@ for (var i=0; i<myObject.length; i++){
     myObject[i][j] = i*10+j;
   }
 }
+
+// convert to string
 var datatext = JSON.stringify(myObject);
 
 function downloadme(){
+  // create a element
   var downloader = document.createElement('a');
+  // required for firefox. ref. https://support.mozilla.org/en-US/questions/968992
+  document.body.appendChild(downloader);
   downloader.setAttribute(
     "href",
     "data:application/json," + encodeURIComponent(datatext));
+    // download attribute : html5 feature, and
+    // seemingly not supported in IE/Safari.
   downloader.setAttribute("download", "data.json");
   downloader.click();
 }
