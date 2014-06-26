9601921dd4583330d5642cf84bd6d379bcdbdc80
lesguillemets <lesguillemets@users.noreply.github.com>
2014-06-26 10:59:33 +0900

    rgbspace?

--- starwars/js/gridCoder.js
+++ starwars/js/gridCoder.js
@@ -1,4 +1,20 @@
 function encodefromArray(ary){
   var flatList = [];
-  flatList.concat.apply(flatList, ary);
+  var encoded = "";
+  var buf, value;
+  flatList = flatList.concat.apply(flatList, ary).concat([0,0]);
+  // 63: ? to 126:~
+  for (var i=0; i<flatList.length-2; i+=3){
+    buf = flatList.slice(i,i+3);
+    value = buf[0]*(4*4) + buf[1]*4 + buf[2]; // ugly
+    encoded += String.fromCharCode(value+63);
+  }
+  return encoded
+}
+
+function decodeArray(code){
+  var size = Math.floor(Math.sqrt(code.length * 3));
+  var ary = new Array();
+  for (var i=0; i<code.length-2; i+=3){
+  }
 }
