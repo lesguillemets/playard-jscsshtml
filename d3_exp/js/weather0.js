912b27030d377f1bf922849ca96974932323ce04
lesguillemets <lesguillemets@users.noreply.github.com>
2014-07-14 02:00:27 +0900

    rain

---
+++ d3_exp/js/weather0.js
@@ -0,0 +1,43 @@
+window.onload = main;
+
+function type(d){
+  d.rain = +d.rain;
+  return d;
+}
+
+function main(){
+  var gridSize = 10;
+  var sqS = 12;
+  var width= sqS * 60;
+  var height = sqS * 7;
+  
+  var x = d3.scale.linear().range([100,20]);
+  
+  var chart = d3.select(".chart")
+      .attr("width", width)
+      .attr("height", height);
+  
+  d3.csv("./data/kochi_rain.csv", type, function(error,data){
+    x.domain([0, d3.max(data, function(d){return d.rain;})]);
+    
+    var grid = chart.selectAll('g')
+      .data(data)
+    .enter().append('g')
+      .attr("transform", function(d,i){
+        var dayOfWeek = i%7;
+        var weekOfYear = Math.floor(i/7);
+        return "translate(" + sqS*weekOfYear + ',' + sqS*dayOfWeek + ")";
+      });
+    
+    grid.append("rect")
+      .attr('width', gridSize).attr('height', gridSize)
+      .style('fill', function(d){
+        if(d.rain > 0){
+          return 'hsl(240,100%,' + x(d.rain) + '%)';
+        }
+        else {
+          return 'hsl(20,20%,90%)';
+        }
+      });
+  });
+}
