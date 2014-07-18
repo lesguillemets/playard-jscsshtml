37d5713a2930036c039a96b65f74d7fb74c0fbb5
lesguillemets <lesguillemets@users.noreply.github.com>
2014-07-19 00:16:32 +0900

    add margin and basic label feature.

--- d3_exp/js/hytherProto.js
+++ d3_exp/js/hytherProto.js
@@ -1,10 +1,11 @@
 window.onload = main;
 
-function formType(converter){
+function formType(tempConv, rainConv){
   return function(d){
     d.rainfall = +d.rainfall;
     d.temperature = +d.temperature;
-    d.tempLoc = Math.floor(converter(d.temperature)*1000)/1000;
+    d.tempLoc = Math.floor(tempConv(d.temperature)*1000)/1000;
+    d.rainLoc = Math.floor(rainConv(d.rainfall)*1000)/1000;
     return d;
   }
 }
@@ -12,57 +13,85 @@ function formType(converter){
 function main(){
   var width=500;
   var height=500;
-  var expectedMinTemp = 0;
-  var expectedMaxTemp = 30;
+  var xmargin=50;
+  var ymargin=50;
+  var expectedMinTemp = -10;
+  var expectedMaxTemp = 40;
+  var expectedMinRainfall = 0;
+  var expectedMaxRainfall = 500;
   
   var tempConverter
-    = d3.scale.linear().range([height,0])
+    = d3.scale.linear().range([height+ymargin,0])
                        .domain([
                          expectedMinTemp,
                          expectedMaxTemp
                        ]);
+  var rainConverter
+    = d3.scale.linear().range([xmargin, xmargin+width])
+                       .domain([
+                         expectedMinRainfall,
+                         expectedMaxRainfall
+                       ]);
   
-  var type = formType(tempConverter);
+  var type = formType(tempConverter, rainConverter);
   var chart = d3.select("body").append("svg")
-      .attr("width", width)
-      .attr("height",height)
-      .attr("class", 'chart');
+      .attr("width", width+2*xmargin)
+      .attr("height",height+2*ymargin)
+      .attr("class", 'chart')
+      .style("overflow", "visible");
   
-  function drawHyther(dataFileName, color){ // {{{
+  function drawHyther(cityname, year, color){ // {{{
+    var dataFileName = "./data/hyther/" + cityname + '/' + year + '.csv';
     d3.csv(
       dataFileName, type,
       function(error,data){
         var city= chart.append("g").attr('id',dataFileName);
         var graphFunc = d3.svg.line()
-        .x(function(d) { return d.rainfall; })
+        .x(function(d) { return d.rainLoc; })
         .y(function(d) { return d.tempLoc; })
         .interpolate("linear-closed");
         
         city.append("path")
         .attr("d", graphFunc(data))
         .style("stroke", color)
+        .style("stroke-width", 1.5)
         .style("fill", 'none');
         
-        var points = city.append("g");
-        points.selectAll('circle').data(data)
-        .enter().append('circle')
-        .attr('cx', function(d){ return d.rainfall;})
-        .attr('cy', function(d){ return d.tempLoc; })
-        .attr('r',4)
-        .style('fill', 'grey');
-
-        var month = city.append("g");
+        // FIXME Z0.0 degrees rendered as 20 degrees
+        var month = city.append("g").attr('id', "monthText");
         month.selectAll('text').data(data)
         .enter().append('text')
-        .attr('x', function(d){ return d.rainfall;})
+        .attr('x', function(d){ return d.rainLoc;})
         .attr('y', function(d){ return d.tempLoc; })
+        .attr('dx', 5)
+        .attr('dy', -5)
+        .attr('title', function(d,i){
+          var yearStr = (year === 'mean' ? '平年' : year + '年');
+          var label = cityname + ", " + yearStr + " " + (i+1) + '月\n';
+          label += d.rainfall + " mm, " + d.temperature + " ℃";
+          return label;
+        })
         .text(function(d,i){ return i+1;});
+        
+        // TODO : points should ALWAYS be on the top.
+        var points = city.append("g").attr('id', "dataPoints");
+        points.selectAll('circle').data(data)
+        .enter().append('circle')
+        .attr('cx', function(d){ return d.rainLoc;})
+        .attr('cy', function(d){ return d.tempLoc; })
+        .attr('r',4)
+        .attr('title', function(d,i){
+          var yearStr = (year === 'mean' ? '平年' : year + '年');
+          var label = cityname + ", " + yearStr + " " + (i+1) + '月\n';
+          label += d.rainfall + " mm, " + d.temperature + " ℃";
+          return label;
+        })
+        .style('fill', 'grey');
       }
     );
   } //}}}
-  drawHyther("data/hyther/京都/2013.csv",'hsl(0,100%,50%)');
-  drawHyther("data/hyther/釧路/2013.csv", 'hsl(180,100%,50%)');
-  drawHyther("data/hyther/京都/mean.csv",'hsl(0,50%,50%)');
-  drawHyther("data/hyther/釧路/mean.csv", 'hsl(180,50%,50%)');
-  drawHyther("data/hyther/新潟/mean.csv",'hsl(270,100%,50%)');
+  drawHyther("京都", 2013,'hsl(0,80%,50%)');
+  drawHyther("高知", 2013, 'hsl(180,80%,50%)');
+  drawHyther("釧路", 2013, 'hsl(90,80%,50%)');
+  drawHyther("新潟", 2013, 'hsl(270,80%,50%)');
 }
