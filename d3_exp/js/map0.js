c5ed484a8c3cf0b129583d787f900d20c7c39056
lesguillemets <lesguillemets@users.noreply.github.com>
2014-08-09 01:00:36 +0900

    japaaaaaaaaaan

--- d3_exp/js/map0.js
+++ d3_exp/js/map0.js
@@ -33,6 +33,39 @@ function main(){
       .attr("class", function(d){return "subunit " + d.id;})
       .style('fill', function(n,i){return colors[i%colors.length]});
     
+    mapsvg.append("g")
+      .selectAll("circle").data(topojson.feature(jp,jp.objects.places).features)
+    .enter().append('circle')
+      .attr('transform', function(d){
+        //console.log(d.geometry.coordinates)
+        return "translate(" + projection(d.geometry.coordinates) + ")";
+      })
+      .attr("class", "city")
+      .attr('r', 2);
+    
+    mapsvg.append("g")
+      .attr("class", 'labels')
+      .selectAll(".label").data(topojson.feature(jp,jp.objects.places).features)
+    .enter().append("text")
+      .attr('class', 'label')
+      .attr('transform', function(d){
+        return "translate(" + projection(d.geometry.coordinates) + ")";
+      })
+      .attr('dy', '0.2em')
+      .attr('font-size', '9px')
+      .text( function(d){ return d.properties.name; });
+    
+    mapsvg.append('g')
+      .attr('class', 'arealabels')
+      .selectAll('.area-label')
+      .data(topojson.feature(jp, jp.objects.subunits).features)
+    .enter().append('text')
+      .attr('class', 'arealabels')
+      .attr('transform', function(d){
+        return 'translate(' + mappath.centroid(d) + ')';
+      })
+      .style('fill', 'rgba(100,100,100,0.8)')
+      .text(function(d) { return d.properties.name ;});
     console.log(jp.objects);
   });
 }
