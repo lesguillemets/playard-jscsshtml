5b958c643301bd41801a2fd190d74ded4b118dc5
lesguillemets <lesguillemets@users.noreply.github.com>
2014-10-09 12:23:13 +0900

    wavy!

--- threejs_exp/js/wavy0.js
+++ threejs_exp/js/wavy0.js
@@ -1,5 +1,6 @@
 (function(){
-  var scene, camera, renderer;
+  var scene, camera, renderer, terra;
+  var t=0;
   var O = new THREE.Vector3(0,0,0);
   window.addEventListener('load', main);
   function main(){
@@ -15,14 +16,14 @@
     renderer.render(scene,camera);
   }
   
-  function setCamera(){
+  function setCamera(){ //{{{
     camera = new THREE.PerspectiveCamera(
       35, window.innerWidth/window.innerHeight, 1, 4000
     );
     camera.position.set(50,100,50);
     camera.up = new THREE.Vector3(0,0,1);
     camera.lookAt(O);
-  }
+  } //}}}
   
   function setRenderer(){ //{{{
     renderer = Detector.webgl?
@@ -37,20 +38,36 @@
   } //}}}
   
   function createWorld(){ //{{{
-    var geo = new THREE.PlaneGeometry(80,80,200,200);
+    var geo = new THREE.PlaneGeometry(80,80,99,99);
+    console.log(geo.vertices.length);
     for (var i=0; i<geo.vertices.length; i++){
-      geo.vertices[i].z = 2*Math.random();
+      geo.vertices[i].z = 2*Math.sin(Math.sqrt(
+        (Math.pow(Math.floor(i/100)-50,2)+ Math.pow(i%100 - 50,2))/10
+      )+ Math.PI*(t/100));
     }
     var material = new THREE.MeshPhongMaterial({
       color: 0xdddddd,
       wireframe: true
     });
     
-    var plane = new THREE.Mesh(geo,material);
-    plane.position.set(0,0,0);
-    scene.add(plane);
+    terra = new THREE.Mesh(geo,material);
+    terra.position.set(0,0,0);
+    scene.add(terra);
   } //}}}
   
   function animate(){
+    requestAnimationFrame(animate);
+    t++;
+    var geo = terra.geometry;
+    for(var i=0; i<geo.vertices.length; i++){
+      geo.vertices[i].z = 2*Math.sin(Math.sqrt(
+        (Math.pow(Math.floor(i/100)-50,2)+ Math.pow(i%100 - 50,2))/10
+      )- Math.PI*(t/50));
+    }
+    geo.verticesNeedUpdate = true;
+    renderer.render(scene,camera);
+    if (t==100){
+      t=0;
+    }
   }
 })();
